f3c85b9fc0004534a0f3ef022cf496c5c98909f9
Sudip Das <sudip5428@gmail.com>
2022-09-23 19:01:59 +0530

    cheanges docker file

--- app.py
+++ app.py
@@ -8,4 +8,6 @@ def hello_world():
     return "<p>Hello, World!!!</p>"
 
 
-app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
