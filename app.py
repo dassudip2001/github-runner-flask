e9ecde4afa71480acfc0cd7283af9055c40a2922
Sudip Das <85440350+dassudip2001@users.noreply.github.com>
2023-05-06 20:54:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!!!</p>"
+    return "<p>Hello, World!</p>"
 
 
+    
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
